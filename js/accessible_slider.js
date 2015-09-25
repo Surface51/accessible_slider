d30d692adebe3c2fdad78f751fce4677e281a8f9
Michelle Darling <michelle@Michelles-MacBook-Pro-9.local>
2015-09-25 10:31:09 -0500

    user should get their own bootstrap, so remove all bootstrap from our module and use external library and libraries module

--- js/accessible_slider.js
+++ js/accessible_slider.js
@@ -1,22 +1,20 @@
 (function ($) {
   Drupal.behaviors.viewsBootstrapCarousel = {
     attach: function(context, settings) {
-    $.getScript( "/sites/all/modules/custom/accessible_slider/js/bootstrap-accessibility.js" )
-	  .done(function( script, textStatus ) {
-	    console.log( textStatus );
-	  })
-	  .fail(function( jqxhr, settings, exception ) {
-	    $( "div.log" ).text( "Triggered ajaxError handler." );
-	});
-      $(function () {
-        $.each(settings.viewsBootstrap.carousel, function(id, carousel) {
-          try {
-            $('#views-bootstrap-carousel-' + carousel.id, context).carousel(carousel.attributes);
-          }
-          catch(err) {
-            console.log(err);
-          }
-        });
+      $.getScript( "/sites/all/modules/custom/accessible_slider/js/bootstrap-accessibility.js" )
+	     .done(function( script, textStatus ) {
+	       console.log( textStatus );
+	     })
+	     .fail(function( jqxhr, settings, exception ) {
+	       $( "div.log" ).text( "Triggered ajaxError handler." );
+	     });
+      $.each(settings.viewsBootstrap.carousel, function(id, carousel) {
+        try {
+          $('#views-bootstrap-carousel-' + carousel.id, context).carousel(carousel.attributes);
+        }
+        catch(err) {
+          console.log(err);
+        }
       });
     }
   };
