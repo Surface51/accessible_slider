194005bfaf3ddcc64c8f9f725c104ed30c65661d
Barry Abrams <barry@surface51.com>
2015-10-07 16:05:17 -0500

    changed js to only trigger if slider is on page

--- js/accessible_slider.js
+++ js/accessible_slider.js
@@ -1,23 +1,26 @@
 (function ($) {
-  Drupal.behaviors.viewsBootstrapCarousel = {
-    attach: function(context, settings) {
-    $.getScript( "/sites/all/modules/custom/accessible_slider/js/bootstrap-accessibility.js" )
-	  .done(function( script, textStatus ) {
-	    console.log( textStatus );
-	  })
-	  .fail(function( jqxhr, settings, exception ) {
-	    $( "div.log" ).text( "Triggered ajaxError handler." );
-	});
-      $(function () {
-        $.each(settings.viewsBootstrap.carousel, function(id, carousel) {
-          try {
-            $('#views-bootstrap-carousel-' + carousel.id, context).carousel(carousel.attributes);
-          }
-          catch(err) {
-            console.log(err);
-          }
+    Drupal.behaviors.viewsBootstrapCarousel = {
+      attach: function(context, settings) {
+       if($(".accessible-slider-plugin-style").length > 0) {
+          $.getScript( "/sites/all/modules/custom/accessible_slider/js/bootstrap-accessibility.js" )
+      	  .done(function( script, textStatus ) {
+      	    console.log( textStatus );
+      	  })
+      	  .fail(function( jqxhr, settings, exception ) {
+      	    $( "div.log" ).text( "Triggered ajaxError handler." );
+      	});
+        $(function () {
+          $.each(settings.viewsBootstrap.carousel, function(id, carousel) {
+            try {
+              $('#views-bootstrap-carousel-' + carousel.id, context).carousel(carousel.attributes);
+            }
+            catch(err) {
+              console.log(err);
+            }
+          });
         });
-      });
+      }
     }
   };
 })(jQuery);
+
